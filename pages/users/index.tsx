61677cfe1dea737231fc39d59f733d714030c44b
IR101D <somefkingadress@gmail.com>
2025-09-20 23:21:31 +0100

    Update index.tsx

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -20,7 +20,7 @@ const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
         </div>
         <div className="grid grid-cols-3 gap-2 ">
           {
-            users?.map(({ id, name, username, email, phone, address, website, company }: UserProps, key: number) => (
+            users.map(({ id, name, username, email, phone, address, website, company }: UserProps, key: number) => (
               <UserCard name={name} username={username} phone={phone} email={email} address={address} website={website} company={company} id={id} key={key} />
             ))
           }
